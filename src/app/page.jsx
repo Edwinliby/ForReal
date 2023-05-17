b0706a61f14023f1035986f24b6358836546efba
Edwinliby <edwinliby30@gmail.com>
2023-05-17 21:23:27 +0530

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Bg from '../components/Bg'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className='text-white text-[3rem]'>
+
+      <Bg/>
       Hello
+      <Hero/>
+      <Footer/>
     </div>
   )
 }
