172c8ed8654059c3364ed9b92a071bb6089ca59f
Edwinliby <edwinliby30@gmail.com>
2023-05-17 21:48:19 +0530

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Bg from '../components/Bg'
-import Footer from '../components/Footer'
+// import Bg from '../components/Bg'
+// import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div className='text-white text-[3rem]'>
 
-      <Bg/>
-      Hello
+      {/* <Bg/> */}
       <Hero/>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function hero() {
     return (
-        <div className='h-screen'>
-            Hello, I am Professsional Web Developer
+        <div className='h-screen text-center flex flex-col justify-center items-center'>
+            Hi, I am Professsional Web Developer
+            <span className='font-bold'>Trust Me😉</span>
         </div>
     )
 }
