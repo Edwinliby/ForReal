1cf9af2f989cab861e7569e12fa1e5d6f2402aa0
Edwinliby <edwinliby30@gmail.com>
2023-07-02 13:27:59 +0530

    added text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default function hero() {
     return (
-        <div className='h-screen text-center flex flex-col justify-center items-center'>
-            Hi, I am Professsional Web Developer
+        <div className='h-screen text-[2rem] lg:text-[4rem] text-center flex flex-col justify-center items-center'>
+            Hi, I am Pro-CSS Web Developer
             <span className='font-bold'>Trust Me😉</span>
+
+            <p className='text-[.8rem] lg:text-[1.1rem]'>
+                Currently I am working on my portfolio website
+                <br />
+                soon I will launch it and show all my <b className='text-[#456FF6]'>freelance projects</b> & <b className='text-[#456FF6]'>casestudies</b>.
+            </p>
         </div>
     )
 }
